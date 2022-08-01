6010f02a2a50d613eb97b46dc8b375f95e18c08d
msanjeetaxioned <sanjeetm@axioned.com>
2022-08-01 20:09:15 +0530

    Animate box1 with scrub behaviour

--- js/horizontal-scroll.js
+++ js/horizontal-scroll.js
@@ -3,35 +3,25 @@ gsap.registerPlugin(ScrollTrigger);
 const body = document.querySelector("body");
 const mainContainer = body.querySelector(".main-container");
 const extraLongContainer = mainContainer.querySelector(".extra-long-container");
-const horizontalCoordinate = body.querySelector(".horizontal-coordinate");
 
-const onScroll = (event) => {
-    const delta = Math.sign(event.deltaY);
-
-    if(delta == 1) {
-        mainContainer.scrollLeft = mainContainer.scrollLeft + 20;
-    } else {
-        mainContainer.scrollLeft = mainContainer.scrollLeft - 20;
-    }
-    horizontalCoordinate.innerText = "x: " + mainContainer.scrollLeft;
-}
-
-document.addEventListener("wheel", onScroll);
-
-let scrollTween = gsap.to(".main-container", {
+let scrollTween = gsap.to(".extra-long-container", {
     xPercent: -100,
-    ease: "none", // <-- IMPORTANT!
+    x: () => window.innerWidth,
+    ease: "none",
     scrollTrigger: {
-      trigger: ".main-container",
-      start: "left left",
-      end: () => "+=" + extraLongContainer.offsetWidth,
-      scrub: 1
+        trigger: ".extra-long-container",
+        start: "top top",
+        end: () => "+=" + extraLongContainer.offsetWidth + "px",
+        scrub: true,
+        pin: true,
+        invalidateOnRefresh: true,
+        anticipatePin: 1
     }
 });
 
 const t1 = gsap.timeline({
     scrollTrigger: {
-        trigger: ".box1",
+        trigger: ".square1",
         start: "left 70%",
         end: "left 20%",
         scrub: 1,
@@ -40,7 +30,7 @@ const t1 = gsap.timeline({
     },
 });
 
-t1.to(".box1", {
+t1.to(".square1", {
     scale: 2,
     rotation: 360,
     duration: 3
